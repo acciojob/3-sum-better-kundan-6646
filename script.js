18f7e27ff0b4d0d9ba215953288921d479fd843e
Kundan Kumar <59140242+kundan-6646@users.noreply.github.com>
2023-05-15 13:47:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
 function threeSum(arr, target) {
-// write your code here
-  
+	let closest = arr[0] + arr[1] + arr[2];
+	for(let i = 0; i < arr.length; i++) {
+		for(let j = i+1; j < arr.length; j++) {
+			for(let k = j+1; k < arr.length; k++){
+				let currSum = arr[i] + arr[j] + arr[k];
+				if(Math.abs(target-closest) > Math.abs(target - currSum)) {
+					closest = currSum;
+				}
+			}
+		}
+	}
+
+	return closest;
 }
 
 module.exports = threeSum;
